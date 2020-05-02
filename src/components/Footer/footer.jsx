182bb9f076bd8c4a0edb45215a45e23641d74237
Rain Tomista <rstomista@up.edu.ph>
2020-05-02 20:29:33 +0800

    Updates

--- src/components/Footer/footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "@components/Footer/footer.module.css"
-
-export default () => (
-  <footer className={styles.footer}>
-    🎨 Made by Rain Tomista 💻
-  </footer>
-)
